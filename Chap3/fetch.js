709cdbca1b0db20a721d950ed2569d12aa08ca4d
SONER PEKER <80073225+C8338Soner@users.noreply.github.com>
2023-07-05 15:07:52 +0300

    Update fetch.js
    node-fetch package version proplems

--- Chap3/fetch.js
+++ Chap3/fetch.js
@@ -1,4 +1,5 @@
-const fetch = require('node-fetch');
+
+const fetch = require('node-fetch');// ı gave this err "File is a CommonJS module; it may be converted to an ES module." and the source of issue is node-fetch package must be install like this" npm install node-fetch@2" 
 
 (async function getData() {
     const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto/");
